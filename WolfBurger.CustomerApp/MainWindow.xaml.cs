0871b85ddaa528e59838abe8ae878d8e13cbd018
Anup Kumar Sinha <anupk_sinha@yahoo.com>
2023-05-03 22:42:38 +0800

    Create 1st View
    created first view as User Control

--- WolfBurger.CustomerApp/MainWindow.xaml.cs
+++ WolfBurger.CustomerApp/MainWindow.xaml.cs
@@ -31,15 +31,6 @@ namespace WolfBurger.CustomerApp
             //btnAddCustomer.Content = "Customer Added!!";
         }
 
-        private void MoveNavigateBtn_Click(object sender, RoutedEventArgs e)
-        {
-            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
-            //var newColumn = column == 0 ? 2 : 0;
-            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
-
-            var column = Grid.GetColumn(CustomerListGrid);
-            var newColumn = column == 0 ? 2: 0;
-            Grid.SetColumn(CustomerListGrid, newColumn);
-        }
+   
     }
 }
---
+++ WolfBurger.CustomerApp/View/CustomerView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WolfBurger.CustomerApp.View
+{
+    public partial class CustomerView : UserControl
+    {
+        public CustomerView()
+        {
+            InitializeComponent();
+        }
+        private void MoveNavigateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+            var newColumn = column == 0 ? 2 : 0;
+            Grid.SetColumn(CustomerListGrid, newColumn);
+        }
+    }
+}
